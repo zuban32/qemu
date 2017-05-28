316c164b49388b666209185396daede508958926
Aleksandr Bezzubikov <zuban32s@gmail.com>
2017-05-28 21:08:33 +0300

    pcie-pci bridge: init->realize, make the device PCIExpress

--- hw/pci-bridge/pcie_pci.c
+++ hw/pci-bridge/pcie_pci.c
@@ -1,4 +1,5 @@
 #include "qemu/osdep.h"
+#include "qapi/error.h"
 #include "hw/pci/pci.h"
 #include "hw/pci/pci_bus.h"
 #include "hw/pci/pci_bridge.h"
@@ -10,11 +11,33 @@ typedef struct PCIEPCIBridge {
 } PCIEPCIBridge;
 
 
-static int pciepci_bridge_initfn(PCIDevice *d)
+static void pciepci_bridge_realize(PCIDevice *d, Error **errp)
 {
+	int rc;
     pci_bridge_initfn(d, TYPE_PCI_BUS);
 
-    return 0;
+    rc = pci_bridge_ssvid_init(d, 0x40, 0, 0x32);
+    if(rc < 0) {
+    	error_setg(errp, "Can't add SSVID, error %d", rc);
+    	goto error;
+    }
+
+    rc = pcie_cap_init(d, 0, PCI_EXP_TYPE_PCI_BRIDGE, 0);
+    if(rc < 0) {
+    	error_setg(errp, "Can't add PCIE-PCI bridge capability, error %d", rc);
+    	goto error;
+    }
+
+    return;
+
+error:
+	pci_bridge_exitfn(d);
+}
+
+static void pciepci_bridge_exit(PCIDevice *d)
+{
+	pcie_cap_exit(d);
+	pci_bridge_exitfn(d);
 }
 
 static const VMStateDescription pciepci_bridge_dev_vmstate = {
@@ -30,10 +53,12 @@ static void pciepci_bridge_class_init(ObjectClass *klass, void *data)
     PCIDeviceClass *k = PCI_DEVICE_CLASS(klass);
     DeviceClass *dc = DEVICE_CLASS(klass);
 
+    k->is_express = 1;
     k->is_bridge = 1;
     k->vendor_id = PCI_VENDOR_ID_REDHAT;
     k->device_id = PCI_DEVICE_ID_REDHAT_PCIE_BRIDGE;
-    k->init = pciepci_bridge_initfn;
+    k->realize = pciepci_bridge_realize;
+    k->exit = pciepci_bridge_exit;
     k->config_write = pci_bridge_write_config;
     dc->vmsd = &pciepci_bridge_dev_vmstate;
     set_bit(DEVICE_CATEGORY_BRIDGE, dc->categories);
